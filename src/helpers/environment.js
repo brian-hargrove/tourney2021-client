61b3040ae188ae7ba00f97490fff9d93d7009537
Brian <brian.hargrove@outlook.com>
2021-03-19 12:19:59 -0400

    environment change

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.localhost) {
   case 'localhost' || '127.0.0.1':
     APIURL = 'http://localhost:3000';
     break;
-  case 'tourney2021.herokuapp.com':
+  case 'tourney2021-server.herokuapp.com':
     APIURL = 'https://tourney2021-server.herokuapp.com';
 }
 
